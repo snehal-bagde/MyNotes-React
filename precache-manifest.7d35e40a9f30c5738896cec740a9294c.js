adec9a66415791fe5323b53ac3b539d359c1c610
snehal-bagde <snehal.t.bagde@gmail.com>
2020-07-07 01:54:58 +0530

    Updates

---
+++ precache-manifest.7d35e40a9f30c5738896cec740a9294c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77271573e5c360e222500a464191eb9",
+    "url": "/MyNotes-React/index.html"
+  },
+  {
+    "revision": "f67400c1b40d9eb3ca97",
+    "url": "/MyNotes-React/static/css/main.d7acada2.chunk.css"
+  },
+  {
+    "revision": "6aec1b0b570606c92b86",
+    "url": "/MyNotes-React/static/js/2.f857c0b7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MyNotes-React/static/js/2.f857c0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67400c1b40d9eb3ca97",
+    "url": "/MyNotes-React/static/js/main.ed225f7d.chunk.js"
+  },
+  {
+    "revision": "ee115158b1415eb812b5",
+    "url": "/MyNotes-React/static/js/runtime-main.91de959c.js"
+  },
+  {
+    "revision": "96a95536b6a37f3a40b39243dda34989",
+    "url": "/MyNotes-React/static/media/logo.96a95536.png"
+  }
+]);
